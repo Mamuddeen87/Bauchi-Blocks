a7fbfec9ad7c8a3fb9550650532a3cc2bd806e00
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2026-02-10 21:32:31 +0100

    Security and Authentication feature added and tested

--- bauchi_blocks/user/views.py
+++ bauchi_blocks/user/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterUserForm
+from .forms import UpdateProfileForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+#redirects urls of home to register.html
+def home(request):
+    return redirect("register")
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = RegisterUserForm()
+    return render(request, "user/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        form = UpdateProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = UpdateProfileForm(instance=request.user)
+    return render(request, "user/profile.html", {"form": form})
