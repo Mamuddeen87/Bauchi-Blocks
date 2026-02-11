9ed5dc2d09da76deacaab3456a570b00ef57caa4
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2026-02-11 16:47:50 +0100

    created all models.py across all the apps

--- bauchi_blocks/user/models.py
+++ bauchi_blocks/user/models.py
@@ -2,9 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
+
+
+    ROLE_CHOICES = [
+            ("CEO", "CEO"),
+            ("MANAGER", "Manager"),
+            ("DEPUTY", "Deputy"),
+            ("OPERATOR", "Operator"),
+            ]
     user = models.OneToOneField(
             User,
             on_delete=models.CASCADE,
+            )
+    role = models.CharField(
+            max_length=20,
+            choices=ROLE_CHOICES,
+            default="OPERATOR"
             )
     bio = models.TextField(blank=True)
     profile_pic = models.ImageField(upload_to = 'profile_pics/', blank=True)
